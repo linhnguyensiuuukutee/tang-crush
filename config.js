71daac0a16109bb394c96972fc9d6bcdee23ce39
linhnguyensiuuukutee <96907506+linhnguyensiuuukutee@users.noreply.github.com>
2021-12-31 19:01:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'gửi chị My đáng juuu của iem',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    chúc chị một năm 2022 zui zeeeeeeee `,
+    introDesc: ` em không biết ở xa v chúc chị sao cho vui nên làm cái này nè =)) đừng có chê á nha
+     `,
     btnIntro: 'hihi',
-    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích emmm :)) <33',
+    title: 'thấy em kutee khonggg',
+    desc: ' màu xanh là không màu đỏ là có =))0',
+    btnYes: 'em thich' chị  :)) <33',
     btnNo: 'ai cho đâu mà không :)) :3',
     question:'này để tặng chị thoiiiii <3',
     btnReply: 'sent to Linhh <3',
-    reply: 'em biết em kutee mà',
-    mess: 'chị lúc nào cũng xinhhhhh',
-    messDesc: '1 năm mới không buồn không khóc nhaaaa',
-    btnAccept: 'ko thì em đau lonfggggg đí',
+    reply: 'chúc chị một năm mới vui vẻ không buồn không khóc kiếm được mụt người xứng đáng với chị, earn more money hơn :)) ',
+    mess: 'chị xinh lắm nên đừng có mè tự ti ',
+    messDesc: '1 năm mới chỉ cười như em nè =)))',
+    btnAccept: 'phải làm được, phải làm được, phải làm được',
     messLink: 'https://www.facebook.com/linhxinkdeppppp/',
 }
